6a8b2c5e8621122bab83672da8fe6655be22b274
luyvannda <luyvannda@gmail.com>
2024-03-24 10:08:56 +0700

    use Array from to generate up to 20 options for select and prevent default behavior of form submit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,30 @@
+import { ReactNode } from "react";
+
 export default function Form() {
+  const options: ReactNode = Array.from({ length: 20 }, (current, index) => (
+    <option
+      value={index + 1}
+      key={index + 1}
+      className=" bg-[#5a3e2b] text-white"
+    >
+      {index + 1}
+    </option>
+  ));
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="flex-center gap-[0.8rem] bg-[#e5771f] px-0 py-[2.8rem]">
+    <form
+      onSubmit={handleSubmit}
+      className="flex-center gap-[0.8rem] bg-[#e5771f] px-0 py-[2.8rem]"
+    >
       <h3 className="mr-[1.6rem] text-[2.4rem]">
         What do you need for your 😍 trip?
       </h3>
       <select className="cursor-pointer rounded-[10rem] bg-[#ffebb3] px-[3.2rem] py-[1.2rem] text-[1.8rem] font-bold text-[#5a3e2b]">
-        <option className="bg-[#5a3e2b] text-white" value="1">
-          1
-        </option>
-        <option className="bg-[#5a3e2b] text-white" value="2">
-          2
-        </option>
-        <option className="bg-[#5a3e2b] text-white" value="3">
-          3
-        </option>
+        {options}
       </select>
       <input
         className="cursor-pointer rounded-[10rem] bg-[#ffebb3] px-[3.2rem] py-[1.2rem] text-[1.8rem] font-bold text-[#5a3e2b]"
